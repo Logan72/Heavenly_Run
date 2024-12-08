227fdcc6ce4a0166689000fca7ef3fc2e0653e83
Napoleon <vova.vu72@gmail.com>
2024-12-08 21:30:17 +0700

    Some commit 0

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
@@ -6,13 +7,48 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField] Transform chunkParent;
     [SerializeField] int chunksNumber;
     [SerializeField] float chunkLength;
+    [SerializeField] float chunkMoveSpeed;
+    List<GameObject> chunks = new List<GameObject>();
 
     void Start()
     {
-        for (int i = 0; i < chunksNumber; i++)
+        SpawnChunks();
+    }
+
+    void Update()
+    {
+        MoveChunks();
+    }
+
+    void SpawnChunks()
+    {
+        chunks.Add(Instantiate(chunkPrefab, transform.position, Quaternion.identity, chunkParent));
+
+        for (int i = 0; i < chunksNumber - 1; i++)
         {
-            Vector3 chunkPosition = transform.position + new Vector3(0, 0, chunkLength * i);
-            Instantiate(chunkPrefab, chunkPosition, Quaternion.identity, chunkParent);
+            SpawnChunk();
         }
+    }
+
+    void MoveChunks()
+    {
+        for (int i = 0; i < chunks.Count; i++)
+        {
+            chunks[i].transform.Translate(-chunkMoveSpeed * Time.deltaTime * transform.forward);            
+        }
+
+        if (chunks.Count != 0 && chunks[0].transform.position.z < Camera.main.transform.position.z - chunkLength)
+        {
+            Destroy(chunks[0]);
+            chunks.RemoveAt(0);
+            SpawnChunk();
+        }
+    }
+
+    void SpawnChunk()
+    {
+        Vector3 newChunkPosition = chunks[chunks.Count - 1].transform.position + new Vector3(0, 0, chunkLength);
+        GameObject newChunk = Instantiate(chunkPrefab, newChunkPosition, Quaternion.identity, chunkParent);
+        chunks.Add(newChunk);
     }
 }
